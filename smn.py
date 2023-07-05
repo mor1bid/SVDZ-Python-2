f031ad63c2022af44d2fac7a8f90d989d3d0991c
De <spark49942@gmail.com>
2023-07-05 03:42:08 +0300

    DZ-smn-34

--- smn.py
+++ smn.py
@@ -1,4 +1,5 @@
 from sys import getsizeof
+import math
 
 # 1
 
@@ -13,7 +14,7 @@ print(data3, "это", type(data3))
 print(data4, "это", type(data4))
 
 # 2
-
+print("\n2.")
 data = [42, 4.2, "Сорок два", True, 42, True]
 for i in range(len(data)):
     check = ''
@@ -21,4 +22,40 @@ for i in range(len(data)):
         check = 'целое'
     elif type(data[i]) is str:
         check = 'строка'
-    print(i + 1, "-", data[i], "адрес:", id(data[i]), ", размер:", getsizeof(data[i]), ", хэш:", hash(data[i]), ",", check)
+    print(i + 1, "-", data[i], "адрес:", id(data[i]), ", размер:", getsizeof(data[i]), ", хэш:", hash(data[i]), check)
+
+# 3
+
+numa = int(input("\n3. Введите число: "))
+numb = numa
+binau = bin(numa)
+octau = oct(numa)
+
+binre = ''
+while numa > 0:
+    binre = str(numa % 2) + binre
+    numa //= 2
+octre = ''
+while numb > 0:
+    octre = str(numb % 8) + octre
+    numb //= 8
+
+print("Двоичный вид\nРезультат А:", binau)
+print("Результат Б:", binre)
+print("Восьмеричный вид\nРезультат А:", octau)
+print("Результат Б:", octre)
+
+# 4
+compute = True
+while (compute):
+    d = float(input("\n4. Введите желаемое значение диаметра (до 1000)\n: "))
+    if d < 1000:
+        r = d / 2
+        s = math.pi * math.sqrt(r)
+        l = 2 * math.pi * r
+        print("Площадь данного круга\n: ", f'{s:.50g}')
+        print("Длина данного круга\n: ", f'{l:.50g}')
+        compute = False
+
+# 5
+
